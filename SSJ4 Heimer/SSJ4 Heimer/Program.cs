840121868ecf16ba03c1eccdd7c6123d71f891e9
SSJ4 <Cobrakraizy@gmail.com>
2014-11-18 19:40:04 +0100

    SSJ4 Heimer
    Added some features

--- SSJ4 Heimer/SSJ4 Heimer/Program.cs
+++ SSJ4 Heimer/SSJ4 Heimer/Program.cs
@@ -26,9 +26,13 @@ namespace SSJ4_Heimer
 		
 		private static List<Spell> SpellList = new List<Spell>();
 
+		private static Spell Q;
+		
         private static Spell W;
 
         private static Spell E;
+        
+        private static Spell R;
 
         private static Menu Config;
 
@@ -45,8 +49,8 @@ namespace SSJ4_Heimer
         private static Items.Item CUT;
 
         private static Items.Item TYM;
-
-        //private static Obj_AI_Hero Player;
+        
+        private static Items.Item ZHO;
         
         
         public static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
@@ -69,27 +73,30 @@ namespace SSJ4_Heimer
 
             
             
-            
+        	Q = new Spell(SpellSlot.Q, 450);
             W = new Spell(SpellSlot.W, 1100);
             E = new Spell(SpellSlot.E, 925);
+            R = new Spell(SpellSlot.R, 100);
             
-
-            W.SetSkillshot(250f, 200, 1400, true, SkillshotType.SkillshotLine);
+			
+            W.SetSkillshot(250f, 200, 1400, false, SkillshotType.SkillshotLine);
             E.SetSkillshot(0.51f, 120, 1200, false, SkillshotType.SkillshotCircle);
-           
-
             
+			
+            SpellList.Add(Q);
             SpellList.Add(W);
             SpellList.Add(E);
+            SpellList.Add(R);
             
             
-             RDO = new Items.Item(3143, 490f);
+            RDO = new Items.Item(3143, 490f);
             HYD = new Items.Item(3074, 175f);
             DFG = new Items.Item(3128, 750f);
             YOY = new Items.Item(3142, 185f);
             BOTK = new Items.Item(3153, 450f);
             CUT = new Items.Item(3144, 450f);
             TYM = new Items.Item(3077, 175f);
+            ZHO = new Items.Item(3157, 1f);
             
             //Menu
             Config = new Menu(Champion, "SSJ4 Heimerdinger", true);
@@ -107,8 +114,19 @@ namespace SSJ4_Heimer
             Config.AddSubMenu(new Menu("Combo", "Combo"));
             Config.SubMenu("Combo").AddItem(new MenuItem("UseWCombo", "Use W")).SetValue(true);
             Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E")).SetValue(true);
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseRCombo", "Use R")).SetValue(true);
             Config.SubMenu("Combo").AddItem(new MenuItem("UseItems", "Use Items")).SetValue(true);
             Config.SubMenu("Combo").AddItem(new MenuItem("ActiveCombo", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));
+            Config.SubMenu("Combo").AddItem(new MenuItem("DropQ", "Drop da Q").SetValue(new KeyBind(32, KeyBindType.Press)));
+            //KS Menu
+            Config.AddSubMenu(new Menu("KS Menu", "KSMenu"));
+            Config.SubMenu("KSMenu").AddItem(new MenuItem("rwKS", "Use R->W for KS")).SetValue(true);
+            Config.SubMenu("KSMenu").AddItem(new MenuItem("KSW", "Use W")).SetValue(true);
+            //Config.SubMenu("KSMenu").AddItem(new MenuItem("KSE", "Use E")).SetValue(true);
+            
+            //Safe Menu
+            Config.AddSubMenu(new Menu("Safe me!", "SafeMenu"));
+            Config.SubMenu("SafeMenu").AddItem(new MenuItem("ZhoUlt", "Zhonyas Turret Ult")).SetValue(true);
             
             Config.AddToMainMenu();
 
@@ -121,31 +139,95 @@ namespace SSJ4_Heimer
             
 
 
-            
+        	if (Config.Item("DropQ").GetValue<KeyBind>().Active)
+        	    {
+        		Q.Cast(Player.Position);
+        	    }
+        	
             if (Config.Item("ActiveCombo").GetValue<KeyBind>().Active)
             {
                 Combo();
             }
+            
+            if (Config.Item("rwKS").GetValue<bool>())
+            {
+            	rwKSCombo();
+            }
            
-
+            //if (Config.Item("rE").GetValue<bool>())
+            //{
+            //	rECombo();
+           // }
+            
+           if (Config.Item("KSW").GetValue<bool>())
+           {
+           	KSW();
+           }
+           
+           if (Config.Item("ZhoUlt").GetValue<bool>())
+           {
+           	ZhoUlt();
+           }
+           
         }
+        
+        private static void ZhoUlt()
+        {
+        	var CurrHP = ObjectManager.Player.Health;
+        	var FullHP = ObjectManager.Player.MaxHealth;
+        	var CritHP = FullHP / 100 * 20;
+        	
+        	var target = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+        	
+        	
+        	if (CurrHP <= CritHP)
+        	{
+        		
+        		if (Q.IsReady() && R.IsReady())
+        		{
+        			R.CastOnUnit(ObjectManager.Player);
+        			Utility.DelayAction.Add(100, () => Q.Cast(Player.Position));
+        			Utility.DelayAction.Add(500, () => ZHO.Cast(ObjectManager.Player));
+        		}
+        		
+        	}
+        	
+        }
+        
         
         private static void Combo()
         {
+        	
+        	
             var target = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Magical);
             if (target == null) return;
 			
+            //var collisionObjects = LeagueSharp.Common.Collision.GetCollision(new List<Vector3> { predictedCastPosition }, new PredictionInput { Delay = 250f, Radius = 200, Speed = 1400 });
+            
+            
+            
             //Combo
-            if (target.IsValidTarget(W.Range) && W.IsReady() && (Config.Item("UseWCombo").GetValue<bool>()))
+            if (W.IsReady() && (Config.Item("UseWCombo").GetValue<bool>()))
                 
             {
-                    W.Cast(target);
-            }
+            	var prediction = W.GetPrediction(target);
+            	if (prediction.Hitchance >= HitChance.High && prediction.CollisionObjects.Count(h => h.IsEnemy && !h.IsDead && h is Obj_AI_Minion) < 2)
+            	{
+                    W.Cast(prediction.CastPosition);
             
+            	}
+            }
             if (target.IsValidTarget(E.Range) && E.IsReady() && (Config.Item("UseECombo").GetValue<bool>()))
             {
-                    E.Cast(target);
+                    E.Cast(target, true, true);
             }
+            
+            
+            
+            
+            
+            
             if (Config.Item("UseItems").GetValue<bool>())
             {
                 if (Player.Distance(target) <= RDO.Range)
@@ -180,6 +262,139 @@ namespace SSJ4_Heimer
             
 
 
+        }
+        
+        
+        private static void KSW()
+        {
+        	var target = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+            
+            var prediction = W.GetPrediction(target);
+            
+            if (W.IsReady())
+            {
+            	
+            	if (target.Health < GetWDamage(target))
+            		
+            
+            {
+            		if (prediction.Hitchance >= HitChance.High && prediction.CollisionObjects.Count(h => h.IsEnemy && !h.IsDead && h is Obj_AI_Minion) < 3)
+            	{
+            			Utility.DelayAction.Add(100, () => W.Cast(prediction.CastPosition));
+            	}
+            		
+                    
+            }
+            }
+        }
+        
+        
+        private static void rwKSCombo()
+        {
+        	var target = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+            
+            var prediction = W.GetPrediction(target);
+            
+            if (W.IsReady() && R.IsReady())
+            {
+            	
+            	if (target.Health < GetRwDamage(target))
+            		
+            
+            {
+            		if (prediction.Hitchance >= HitChance.High && prediction.CollisionObjects.Count(h => h.IsEnemy && !h.IsDead && h is Obj_AI_Minion) < 3)
+            	{
+                   		R.CastOnUnit(ObjectManager.Player);
+                   		
+            			Utility.DelayAction.Add(100, () => W.Cast(prediction.CastPosition));
+            	}
+            		
+                    
+            }
+            }
+        }
+        
+        private static void rECombo()
+        {
+        	var target = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Magical);
+            if (target == null) return;
+            
+            var prediction = E.GetPrediction(target);
+        	
+        	if (R.IsReady() && E.IsReady() && (Config.Item("UseRCombo").GetValue<bool>()) && (Config.Item("UseECombo").GetValue<bool>()))
+                {
+                	
+                	if (prediction.Hitchance >= HitChance.High && prediction.AoeTargetsHit.Count(h => h.IsEnemy && !h.IsDead) >= 2)
+                	{
+                		R.CastOnUnit(ObjectManager.Player);
+                		Utility.DelayAction.Add(100, () => E.Cast(prediction.CastPosition));
+                	}
+                }
+        	
+        }
+        
+        
+        private static float GetWDamage(Obj_AI_Base enemy)
+        {
+        	double damage = 0d;
+        	
+        	if (DFG.IsReady())
+                damage += Player.GetItemDamage(enemy, Damage.DamageItems.Dfg)/1.2;
+        	
+        	if (W.IsReady())
+                damage += Player.GetSpellDamage(enemy, SpellSlot.W);
+        	
+        	if (DFG.IsReady())
+                damage = damage*1.2;
+        	
+        	
+            return (float) damage;
+        }
+        
+        private static float GetRwDamage(Obj_AI_Base enemy)
+        {
+        	double damage = 0d;
+        	
+        	if (DFG.IsReady())
+                damage += Player.GetItemDamage(enemy, Damage.DamageItems.Dfg)/1.2;
+        	
+        	if (W.IsReady())
+                damage += Player.GetSpellDamage(enemy, SpellSlot.W, 1)*2;
+        	
+        	if (DFG.IsReady())
+                damage = damage*1.2;
+        	
+        	damage = damage*2;
+            return (float) damage;
+        }
+        
+        private static float GetComboDamage(Obj_AI_Base enemy)
+        {
+            double damage = 0d;
+
+            if (DFG.IsReady())
+                damage += Player.GetItemDamage(enemy, Damage.DamageItems.Dfg)/1.2;
+
+            if (Q.IsReady())
+                damage += Player.GetSpellDamage(enemy, SpellSlot.Q) + Player.GetSpellDamage(enemy, SpellSlot.Q, 1);
+
+            if (W.IsReady())
+                damage += Player.GetSpellDamage(enemy, SpellSlot.W);
+
+            if (E.IsReady())
+                damage += Player.GetSpellDamage(enemy, SpellSlot.E);
+
+            if (R.IsReady())
+                damage += Player.GetSpellDamage(enemy, SpellSlot.R)*8;
+
+            if (DFG.IsReady())
+                damage = damage*1.2;
+
+            
+
+            return (float) damage;
         }
         
         
