ff444fa7ffac23fbbf01d29d01161f368b2dd224
Zytox <Dmn013@hotmail.com>
2016-06-20 17:29:11 +0200

    test

---
+++ Hello World/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.PrintChat("SSJ4 In da building!");
+        }
+    }
+}
